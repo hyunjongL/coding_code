77a4fd64da89743d3f1fb29d4e2d1a2ada047537
Hyunjong LEE <hyunjongl@naver.com>
2017-09-16 01:46:19 +0900

    made into functions now writes to a file names ./code.py now has a print line at the end of the code

--- coding_code.py
+++ coding_code.py
@@ -1,14 +1,16 @@
 import random
 
-code = ''
 variables = ['a', 'b', 'c']
 operation = ['+', '-']
 
-for i in variables:
-    code += i
-    code += ' = '
-    code += str(random.randrange(-100,100,1))
-    code += '\n'
+def var_init():
+    code =''
+    for i in variables:
+        code += i
+        code += ' = '
+        code += str(random.randrange(-100,100,1))
+        code += '\n'
+    return code
 
 def line_of_code():
     line = ''
@@ -22,8 +24,16 @@ def line_of_code():
     line += '\n'
     return line
 
-for i in range(10):
-    code = code + line_of_code()
+def write_codes(n):
+    code = var_init()
+    for i in range(n):
+        code += line_of_code()
+    code += "print(a, b, c)"
+    return code
 
+a = write_codes(random.randrange(7,15,1))
+print(a)
 
-print(code)
+f = open("./code.py", 'w')
+f.write(a)
+f.close
