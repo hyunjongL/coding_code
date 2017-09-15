97424f06d0757a59c1d164bfcfffad4a77114228
Hyunjong LEE <hyunjongl@naver.com>
2017-09-16 02:23:12 +0900

    Added a function 'fill_line' to make making a line easier Made it to be expandable. More variables are okay with the code. Now need to work on how to prevent from making an infinite loop.

--- coding_code.py
+++ coding_code.py
@@ -1,7 +1,11 @@
 import random
 
-variables = ['a', 'b', 'c']
+
+variables = ['a', 'b', 'c', 'd', 'e']
 operation = ['+', '-']
+
+var_num = len(variables)
+op_num = len(operation)
 
 def var_init():
     code =''
@@ -12,10 +16,27 @@ def var_init():
         code += '\n'
     return code
 
-def line_of_code(depth):
-    line = ''
-    if(random.random()>0.8):
-        line += '    '*depth
+def fill_line(depth, type):
+    line = '    ' * depth
+    if type == 0:# var = var x var
+        line += variables[random.randrange(0, var_num, 1)]
+        line += ' = '
+        line += variables[random.randrange(0, var_num, 1)]
+        line += ' '
+        line += operation[random.randrange(0, op_num, 1)]
+        line += ' '
+        line += variables[random.randrange(0, var_num, 1)]
+        line +='\n'
+    elif type == 1:# var = var x num
+        line += variables[random.randrange(0, var_num, 1)]
+        line += ' = '
+        line += variables[random.randrange(0, var_num, 1)]
+        line += ' '
+        line += operation[random.randrange(0, op_num, 1)]
+        line += ' '
+        line += str(random.randrange(0,100,1))
+        line += '\n'
+    elif type == 2:# add a line of if
         line += 'if '
         line += variables[random.randrange(0,3,1)]
         line += ' '
@@ -23,24 +44,28 @@ def line_of_code(depth):
         line += ' '
         line += variables[random.randrange(0,3,1)]
         line += ':\n'
-        depth += 1
-    elif(random.random()>0.9):
-        line += '    '*depth
+    elif type == 3:# add a line of while
         line += 'while '
         line += variables[random.randrange(0,3,1)]
+        line += ' '
         line += ['>', '<'][random.randrange(0,2,1)]
+        line += ' '
         line += variables[random.randrange(0,3,1)]
         line += ':\n'
+    return line
+
+def line_of_code(depth):
+    line =''
+    if(random.random()>0.8):
+        line = fill_line(depth, 2)
         depth += 1
-    line += '    '*depth
-    line += variables[random.randrange(0,3,1)]
-    line += ' = '
-    line += variables[random.randrange(0,3,1)]
-    line += ' '
-    line += operation[random.randrange(0,2,1)]
-    line += ' '
-    line += variables[random.randrange(0,3,1)]
-    line += '\n'
+    elif(random.random()>0.9):
+        line = fill_line(depth, 3)
+        depth += 1
+    if(random.random()>0.4):
+        line += fill_line(depth, 0)
+    else:
+        line += fill_line(depth, 1)
     return (line,depth)
 
 def write_codes(n):
@@ -52,7 +77,13 @@ def write_codes(n):
         line = line_of_code(depth)
         depth = line[1]
         code += line[0]
-    code += "print(a, b, c)"
+    code += 'print('
+    code += variables[0]
+    for i in range(var_num-1):
+        code += ', '
+        code += variables[i+1]
+    code += ')'
+
     return code
 
 a = write_codes(random.randrange(7,15,1))
