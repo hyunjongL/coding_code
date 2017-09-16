73e8e2903634dd1b59fb15dd52f2182d13d36531
Hyunjong LEE <hyunjongl@naver.com>
2017-09-16 18:02:53 +0900

    Now it dynamically prints the code as if it is written in real time. It also has 2 new operations * and /

--- coding_code.py
+++ coding_code.py
@@ -1,12 +1,43 @@
 import random
+import time
 
 imports = ['random']
 variables = ['a', 'b', 'c', 'd', 'e']
-operation = ['+', '-']
+operation = ['+', '-', '*', '/']
 
 import_num = len(imports)
 var_num = len(variables)
 op_num = len(operation)
+
+def random_var():
+    return variables[random.randrange(0, var_num, 1)]
+
+def print_random_var():
+    var = random_var()
+    time.sleep(random.random()/8)
+    print(var, end = '', flush=True)
+    time.sleep(random.random()/8)
+    return var
+
+def random_op():
+    return operation[random.randrange(0, op_num, 1)]
+
+def print_random_op():
+    op = random_op()
+    time.sleep(random.random()/8)
+    print(op, end = '', flush=True)
+    time.sleep(random.random()/8)
+    return op
+
+def random_num(n, m):
+    return str(random.randrange(n, m, 1))
+
+def print_random_num(n, m):
+    num = random_num(n, m)
+    time.sleep(random.random()/8)
+    print(num, end = '', flush=True)
+    time.sleep(random.random()/8)
+    return num
 
 def import_init():
     if import_num == 0:
@@ -14,57 +45,103 @@ def import_init():
     else:
         code = ''
         for i in imports:
+            print('import ', end = '', flush=True)
             code += 'import '
+            print(i, end = '', flush=True)
             code += i
             code += '\n'
+        print('\n', flush=True)
         code += '\n'
         return code
 
 def var_init():
+    comment = '# variable initialization'
+    for i in comment:
+        print(i, end = '', flush = True)
+        time.sleep(random.random()/30)
+    print()
     code ='# variable initialization\n'
     for i in variables:
+        print(i , end = '')
         code += i
+        print(' = ', end = '')
         code += ' = '
-        code += str(random.randrange(-100,100,1))
+        code += print_random_num(1, 100)
+        print('\n', end = '')
         code += '\n'
     code += '\n'
     return code
 
 def fill_line(depth, type):
     line = '    ' * depth
+    for i in range(depth):
+        print('    ', end = '', flush=True)
+        time.sleep(random.random()/20)
     if type == 0:# var = var x var
-        line += variables[random.randrange(0, var_num, 1)]
+        line += print_random_var()
+
+        print(' = ', end = '')
         line += ' = '
-        line += variables[random.randrange(0, var_num, 1)]
+
+        line += print_random_var()
+
+        print(' ', end = '')
         line += ' '
-        line += operation[random.randrange(0, op_num, 1)]
+
+        line += print_random_op()
+
+        print(' ', end = '')
         line += ' '
-        line += variables[random.randrange(0, var_num, 1)]
+        line += print_random_var()
+
+        print('\n', end = '')
         line +='\n'
     elif type == 1:# var = var x num
-        line += variables[random.randrange(0, var_num, 1)]
+        line += print_random_var()
+        print(' = ', end = '')
         line += ' = '
-        line += variables[random.randrange(0, var_num, 1)]
+        line += print_random_var()
+        print(' ', end = '')
         line += ' '
-        line += operation[random.randrange(0, op_num, 1)]
+        line += print_random_op()
+        print(' ', end = '')
         line += ' '
-        line += str(random.randrange(0,100,1))
+        line += print_random_num(1,100)
+        print('\n', end = '')
         line += '\n'
     elif type == 2:# add a line of if
+        print('if ', end = '')
         line += 'if '
-        line += variables[random.randrange(0,3,1)]
+        line += print_random_var()
+
+        print(' ', end = '')
         line += ' '
-        line += ['>', '<'][random.randrange(0,2,1)]
+
+        time.sleep(random.random()/10)
+        a = ['>', '<'][random.randrange(0,2,1)]
+        print(a, end = '', flush=True)
+        line += a
+
+        print(' ', end = '')
         line += ' '
-        line += variables[random.randrange(0,3,1)]
+        line += print_random_var()
+        print(':\n', end = '')
         line += ':\n'
     elif type == 3:# add a line of while
+        print('while ', end = '')
         line += 'while '
-        line += variables[random.randrange(0,3,1)]
+        line += print_random_var()
+        print(' ', end = '')
         line += ' '
-        line += ['>', '<'][random.randrange(0,2,1)]
+        time.sleep(random.random()/10)
+        a = ['>', '<'][random.randrange(0,2,1)]
+        print(a, end = '')
+        line += a
+        time.sleep(random.random()/15)
+        print(' ', end = '', flush=True)
         line += ' '
-        line += variables[random.randrange(0,3,1)]
+        line += print_random_var()
+        print(':\n', end = '')
         line += ':\n'
     return line
 
@@ -103,7 +180,7 @@ def write_codes(n):
     return code
 
 a = write_codes(random.randrange(7,15,1))
-print(a)
+#print(a)
 
 f = open("./code.py", 'w')
 f.write(a)
